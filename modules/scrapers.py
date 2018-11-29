09765db6448a731982d62d1ba152b4c0d7deba98
pkp1337x <20831512+pkp1337x@users.noreply.github.com>
2018-11-29 23:52:46 +0530

    some fixes and additions in scraping

--- modules/scrapers.py
+++ modules/scrapers.py
@@ -63,28 +63,74 @@ async def imdb(e):
     page = requests.get("https://www.imdb.com/find?ref_=nv_sr_fn&q="+final_name+"&s=all")
     lnk = str(page.status_code)
     soup = bs4.BeautifulSoup(page.content,'lxml')
-    results = soup.findAll("td","result_text")
-    mov_title = results[0].text
-    mov_link = "http://www.imdb.com/"+results[0].a['href'] 
+    odds = soup.findAll("tr","odd")
+    mov_title = odds[0].findNext('td').findNext('td').text
+    mov_link = "http://www.imdb.com/"+odds[0].findNext('td').findNext('td').a['href'] 
     page1 = requests.get(mov_link)
     soup = bs4.BeautifulSoup(page1.content,'lxml')
-    story_line = soup.find('div', "inline canwrap")
-    story_line = story_line.findAll("p")[0].text
+    if soup.find('div','poster'):
+    	poster = soup.find('div','poster').img['src']
+    else:
+    	poster = ''
+    if soup.find('div','title_wrapper'):
+    	pg = soup.find('div','title_wrapper').findNext('div').text
+    	mov_details = re.sub(r'\s+',' ',pg)
+    else:
+    	mov_details = ''
+    credits = soup.findAll('div', 'credit_summary_item')
+    if len(credits)==1:
+    	director = credits[0].a.text
+    	writer = 'Not available'
+    	stars = 'Not available'
+    elif len(credits)>2:
+    	director = credits[0].a.text
+    	writer = credits[1].a.text
+    	actors = []
+    	for x in credits[2].findAll('a'):
+    		actors.append(x.text)
+    	actors.pop()
+    	stars = actors[0]+','+actors[1]+','+actors[2]
+    else:
+    	director = credits[0].a.text
+    	writer = 'Not available'
+    	actors = []
+    	for x in credits[1].findAll('a'):
+    		actors.append(x.text)
+    	actors.pop()
+    	stars = actors[0]+','+actors[1]+','+actors[2]	 
+    if soup.find('div', "inline canwrap"):
+    	story_line = soup.find('div', "inline canwrap").findAll('p')[0].text
+    else:
+    	story_line = 'Not available'
     info = soup.findAll('div', "txt-block")
-    for node in info:
-      a = node.findAll('a')
-      for i in a:
-        if "country_of_origin" in i['href']:
-          mov_country = i.string
-    for node in info:
-      a = node.findAll('a')
-      for i in a:
-        if "primary_language" in i['href']:
-          mov_language = i.string
-    rating = soup.findAll('div',"ratingValue")
-    for r in rating:
-      mov_rating = r.strong['title']
-    await e.edit('**Title : **`'+mov_title+'`\n**Rating : **`'+mov_rating+'`\n**Country : **`'+mov_country+'`\n**Language : **`'+mov_language+'`\n**IMDB Url : **`'+mov_link+'`\n**Story Line : **`'+story_line+'`')
+    if info:
+    	mov_country = []
+    	mov_language = []
+    	for node in info:
+    		a = node.findAll('a')
+    		for i in a:
+    			if "country_of_origin" in i['href']:
+    				mov_country.append(i.text)
+    			elif "primary_language" in i['href']:
+    				mov_language.append(i.text) 
+    if soup.findAll('div',"ratingValue"):
+    	for r in soup.findAll('div',"ratingValue"):
+    		mov_rating = r.strong['title']
+    else:
+    	mov_rating = 'Not available'
+    await e.edit('<a href='+poster+'>&nbsp;</a>\n'
+    			'<b>Title : </b><code>'+mov_title+
+    			'</code>\n<code>'+mov_details+
+    			'</code>\n<b>Rating : </b><code>'+mov_rating+
+    			'</code>\n<b>Country : </b><code>'+mov_country[0]+
+    			'</code>\n<b>Language : </b><code>'+mov_language[0]+
+    			'</code>\n<b>Director : </b><code>'+director+
+    			'</code>\n<b>Writer : </b><code>'+writer+
+    			'</code>\n<b>Stars : </b><code>'+stars+
+    			'</code>\n<b>IMDB Url : </b>'+mov_link+
+    			'\n<b>Story Line : </b>'+story_line,
+    			link_preview = True , parse_mode = 'HTML'
+    			)
 @bot.on(events.NewMessage(outgoing=True,pattern='.ly (.*)'))
 @bot.on(events.MessageEdited(outgoing=True,pattern='.ly (.*)'))
 async def ly(e):
